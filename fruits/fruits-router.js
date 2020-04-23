27d0ad5024eaaa538a293f15ed7bc2463d241cef
YasirHasn9 <hassanyasir223@gmail.com>
2020-04-23 15:07:00 -0700

    db3

--- fruits/fruits-router.js
+++ fruits/fruits-router.js
@@ -1,39 +1,41 @@
-const express = require('express');
-const knex = require('knex');
+const express = require("express");
+const knex = require("knex");
 
 const db = knex({
-  client: 'sqlite3',
+  client: "sqlite3",
   connection: {
-    filename: './data/produce.db3'
+    filename: "./data/produce.db3"
   },
   useNullAsDefault: true
 });
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  db('fruits')
-  .then(fruits => {
-    res.json(fruits); 
-  })
-  .catch (err => {
-    res.status(500).json({ message: 'Failed to retrieve fruits' });
-  });
+router.get("/", (req, res) => {
+  db("fruits")
+    .then(fruits => {
+      res.json(fruits);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to retrieve fruits" });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const { id } = req.params;
 
-  db('fruits').where({ id }).first()
-  .then(fruit => {
-    res.json(fruit);
-  }) 
-  .catch (err => {
-    res.status(500).json({ message: 'Failed to retrieve fruit' });
-  });
+  db("fruits")
+    .where({ id })
+    .first()
+    .then(fruit => {
+      res.json(fruit);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to retrieve fruit" });
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/",  (req, res) => {
   const fruitData = req.body;
   db('fruits').insert(fruitData)
   .then(ids => {
@@ -46,6 +48,16 @@ router.post('/', (req, res) => {
     console.log('POST error', err);
     res.status(500).json({ message: "Failed to store data" });
   });
+
+  // try {
+  //   const fruitData = req.body;
+  //   let [id] = await db("fruits").insert(fruitData);
+  //   const newFruit = await db("fruits")
+  //     .where({ id });
+  //   res.status(201).json(newFruit);
+  // } catch (err) {
+  //   next(err);
+  // }
 });
 
 module.exports = router;
